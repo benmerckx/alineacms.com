002fa7c8b66a3f38752d95b72f09fe8ab9a969e6
Ben Merckx <ben@codeurs.be>
2023-06-22 09:44:57 +0200

    web2 => web

---
+++ src/cms.tsx
@@ -0,0 +1,43 @@
+import alinea, {createNextCMS} from 'alinea'
+import {MediaLibrary} from 'alinea/core/media/MediaSchema'
+import {IcRoundPermMedia} from 'alinea/ui/icons/IcRoundPermMedia'
+import * as schema from './schema'
+
+export const pages = alinea.root('Pages', {
+  index: alinea.page(schema.Home),
+  roadmap: alinea.page(schema.Page),
+  docs: alinea.page(schema.Docs),
+  [alinea.meta]: {
+    contains: ['Page']
+  }
+})
+
+export const cms = createNextCMS({
+  dashboard: {
+    dashboardUrl: '/admin.html',
+    handlerUrl: '/api/cms',
+    staticFile: '../public/admin.html'
+  },
+  schema,
+  workspaces: {
+    main: alinea.workspace('Alinea website', {
+      pages,
+      media: alinea.root('Media', {
+        media: alinea.page(MediaLibrary),
+        [alinea.meta]: {
+          contains: ['MediaLibrary'],
+          icon: IcRoundPermMedia
+        }
+      }),
+      [alinea.meta]: {
+        color: '#3F61E8',
+        mediaDir: '../public',
+        source: '../content'
+      }
+    })
+  },
+  preview:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/api/preview'
+      : ''
+})
